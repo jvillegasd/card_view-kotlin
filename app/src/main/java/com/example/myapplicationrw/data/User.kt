4040c8ac3cf04d1167d7798be7641db44a5287c0
C9-LinkRs <linkrsfan@gmail.com>
2020-02-21 08:56:33 -0500

    Example finished

--- app/src/main/java/com/example/myapplicationrw/data/User.kt
+++ app/src/main/java/com/example/myapplicationrw/data/User.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class User (val nombre: String, val apellido: String, val email: String, val telefono: String, val titulo: String) :
+data class User (val nombre: String, val apellido: String, val email: String, val telefono: String, val titulo: String, val image_url: String) :
     Parcelable {
 
 }
